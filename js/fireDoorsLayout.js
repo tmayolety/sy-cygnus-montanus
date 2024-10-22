be978fb430e830351ea17c193a03b56cd93abc7d
Tomeu <tomeu.mayol@etechyachting.com>
2024-10-22 13:09:21 +0200

    Fix SVG

--- js/fireDoorsLayout.js
+++ js/fireDoorsLayout.js
@@ -4,7 +4,5 @@ var screen = {
 };
 
 
-$("#FireDoorLayout_1").load("src/ga/decks/GO_bridgeDeck.svg");
-$("#FireDoorLayout_2").load("src/ga/decks/GO_ownerDeck.svg");
-$("#FireDoorLayout_3").load("src/ga/decks/GO_mainDeck.svg");
-$("#FireDoorLayout_4").load("src/ga/decks/GO_lowerDeck.svg");
+$("#FireDoorLayout_1").load("src/ga/decks/upperSaloon.svg");
+$("#FireDoorLayout_2").load("src/ga/decks/mainDeck.svg");
