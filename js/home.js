fa4c1408dfa9688960210c4a7990cca0780610d3
Tomeu Mayol <tomeu.mayol@etechyachting.com>
2025-03-20 13:46:33 +0100

    Refactor homeApp initialization by moving it from home.js to index.html

--- js/home.js
+++ js/home.js
@@ -1,13 +1,3 @@
-var screen = {
-    renderData: function (msg) {
-    }
-}
 
-if (typeof homeMounted == 'undefined') {
-    const homeApp = Vue.createApp({})
-    homeApp.config.compilerOptions.isCustomElement = tag => tag.startsWith('font')
-    homeApp.component('gauge-render', components.gauge)
-    homeApp.mount('.homeApp')
-    homeMounted = true;
-}
+
 
