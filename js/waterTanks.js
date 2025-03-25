1c09753c75b140ae539ba0c86170e18b97ad18af
Tomeu Mayol <tomeu.mayol@etechyachting.com>
2025-03-25 11:38:57 +0100

    test timelines

--- js/waterTanks.js
+++ js/waterTanks.js
@@ -88,7 +88,7 @@ function addCurrentDataPoint() {
 loadDataFromLocalStorage();
 
 // Añadir nuevo punto cada 30 minutos
-setInterval(addCurrentDataPoint, 30 * 60 * 1);
+setInterval(addCurrentDataPoint, 30 * 60 * 100);
 
 // Para pruebas: añadir cada 5 segundos
 // setInterval(addCurrentDataPoint, 5000);
