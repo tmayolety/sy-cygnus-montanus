22f90891aead2d6d573336bf7739373937efed99
Tomeu Mayol <tomeu.mayol@etechyachting.com>
2025-05-20 14:24:31 +0200

    Test notification when alarmType changes

--- js/notifications/notifications.js
+++ js/notifications/notifications.js
@@ -1,37 +1,42 @@
 var notifications = {
-    pupUpNotification(item) {
-        if (FLOATING_NOTIFICATIONS && (
-            (item.alarmTriggered == 1 && item.Status == 9) ||
-            (item.alarmTriggered == 1 && item.Status == 2 && alarmData[parseInt(item.alarmId)].alarmType == 1)
-        )) {
+  pupUpNotification(item) {
+    if (
+      FLOATING_NOTIFICATIONS &&
+      ((item.alarmTriggered == 1 && item.Status == 9) ||
+        (item.alarmTriggered == 1 &&
+          item.Status == 2 &&
+          alarmData[parseInt(item.alarmId)].alarmType == 1))
+    ) {
+      const alarmId = parseInt(item.alarmId);
+      const alarm = alarmData[alarmId];
+      const alarmName = alarm?.alarmDescription || "Unnamed Alarm";
 
-            const alarm = alarmData[parseInt(item.alarmId)];
-            const alarmName = alarm.alarmDescription;
+      let alarmType = typeof item.alarmType !== "undefined" ? parseInt(item.alarmType) : alarm?.alarmType;
 
-            let notificationColor = "orange";
-            let notificationIcon = "error";
-            let notificationGroup = "alarms";
-            let notificationMessage = "New Pre-Alarm";
+      let notificationColor = "orange";
+      let notificationIcon = "error";
+      let notificationGroup = "alarms";
+      let notificationMessage = "New Pre-Alarm";
 
-            if (alarm.alarmType == 0) {
-                notificationColor = "red";
-                notificationIcon = "warning";
-                notificationGroup = "pre-alarms";
-                notificationMessage = "New Alarm";
-            }
+      if (alarmType == 0) {
+        notificationColor = "red";
+        notificationIcon = "warning";
+        notificationGroup = "pre-alarms";
+        notificationMessage = "New Alarm";
+      }
 
-            if (typeof Quasar !== 'undefined' && Quasar.Notify) {
-                Quasar.Notify.create({
-                    color: notificationColor,
-                    message: `${notificationMessage}: ${alarmName}`,
-                    icon: notificationIcon,
-                    position: 'top',
-                    timeout: 3000,
-                    group: notificationGroup
-                });
-            } else {
-                console.warn("Quasar.Notify no está disponible");
-            }
-        }
+      if (typeof Quasar !== "undefined" && Quasar.Notify) {
+        Quasar.Notify.create({
+          color: notificationColor,
+          message: `${notificationMessage}: ${alarmName}`,
+          icon: notificationIcon,
+          position: "top",
+          timeout: 3000,
+          group: notificationGroup,
+        });
+      } else {
+        console.warn("Quasar.Notify no está disponible");
+      }
     }
+  },
 };
