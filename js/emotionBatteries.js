bc46f782b6e82f9dec175972a2b55a597f107bc5
Tomeu Mayol <tomeu.mayol@etechyachting.com>
2025-05-20 12:30:06 +0200

    UPDATE value mode to 'escalated' for gauge components in hydraulics and emotion batteries

--- js/emotionBatteries.js
+++ js/emotionBatteries.js
@@ -9,6 +9,7 @@ if (typeof emotionBatteriesMounted == 'undefined') {
     const emotionBatteriesApp = Vue.createApp({})
     emotionBatteriesApp.config.compilerOptions.isCustomElement = tag => tag.startsWith('font')
     emotionBatteriesApp.component('gauge-render', components.gauge)
+     emotionBatteriesApp.component('icon-text-value-digital-line-render', components.iconTextValueDigitalLine)
     emotionBatteriesApp.mount('.emotionBatteriesApp')
     emotionBatteriesMounted = true;
 
